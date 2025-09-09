f12d3b0d4f9fcdcfa4e2e2b83dd931c0bd6b8c26
elmohamdi <elmohamdi.77@gmail.com>
2025-09-09 06:34:54 +0300

    deploy flutter web build

--- lib/model/project_model.dart
+++ lib/model/project_model.dart
@@ -7,11 +7,6 @@ class Project {
 }
 
 List<Project> projectList = [
-  Project(
-    'Coffee Application',
-    'Presenting "Coffee" - a Flutter UI application that invites you to indulge in a delightful coffee adventure. Immerse yourself in the world of rich aroma and flavor as you browse through an exquisite selection of specialty coffees. With a user-friendly interface, exploring different blends and discovering their unique details becomes a delightful experience.',
-    'assets/images/coffee.png',
-    'https://github.com/Hamad-Anwar/Coffe-Shop-Beautifull-UI',
-  ),
+
  
 ];
